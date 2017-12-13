d8a343e00e8680976957aac8299c31b7173d9707
Janez Erzen <je1468@student.uni-lj.si>
2017-12-13 12:06:37 +0100

    Add info endpoint

--- src/main/java/com/fri/rso/fririders/notifications/controller/NotificationsController.java
+++ src/main/java/com/fri/rso/fririders/notifications/controller/NotificationsController.java
@@ -5,10 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "v1/notifications")
@@ -30,5 +30,34 @@ public class NotificationsController {
         }
         return ResponseEntity.ok(String.format("Notified %s: '%s'.", recipient, message));
 
+    }
+
+    @RequestMapping(value = "info", method = RequestMethod.GET)
+    public ResponseEntity<String> friRidersInfo() {
+        String data = "{" +
+                "\"clani\": [\"ts4293\", \"ub6189\", \"je1468\"]," +
+                "\"opis_projekta\": \"Nas projekt implementira aplikacijo za oddajo nepremicnin.\"," +
+                "\"mikrostoritve\": " +
+                "[\"http://169.51.16.54:30735/v1/users\"," +
+                " \"http://169.51.16.54:31558/v1/bookings\"," +
+                " \"http://169.51.16.54:31062/v1/accommodations\"]," +
+                " \"http://169.51.16.54:32353/notifications/info\"]," +
+                "\"github\": " +
+                "[\"https://github.com/fri-riders/users\"," +
+                " \"https://github.com/fri-riders/accommodations\"," +
+                " \"https://github.com/fri-riders/display-bookings\"," +
+                " \"https://github.com/fri-riders/notifications\"]," +
+                "\"travis\": " +
+                "[\"https://travis-ci.org/fri-riders/users\"," +
+                " \"https://travis-ci.org/fri-riders/accommodations\"," +
+                " \"https://travis-ci.org/fri-riders/display-bookings\"," +
+                " \"https://travis-ci.org/fri-riders/notifications\"]," +
+                "\"dockerhub\":" +
+                " [\"https://hub.docker.com/r/tomisebjanic/rso-users\"," +
+                " \"https://hub.docker.com/r/janerz6/accommodations\"," +
+                " \"https://hub.docker.com/r/urosbajc/display-bookings\"," +
+                " \"https://hub.docker.com/r/janerz6/notifications\"]" +
+                "}";
+        return ResponseEntity.ok(data);
     }
 }
